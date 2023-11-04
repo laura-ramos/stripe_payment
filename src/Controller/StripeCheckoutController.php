80b939086615e9838afe41bf325a7c20a51ec85e
lauraRa <ramoslaura418@gmail.com>
2023-11-04 13:24:57 -0600

    process successful payment

---
+++ src/Controller/StripeCheckoutController.php
@@ -0,0 +1,212 @@
+<?php
+
+/**
+ * @file
+ * description 
+ */
+
+namespace Drupal\stripe_payment\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Database;
+
+class StripeCheckoutController extends ControllerBase
+{
+  public function success(){
+    // Set your Stripe API secret key
+    $config = $this->config('stripe_payment.settings');
+    $secret_key = $config->get('secret_key');
+    $stripe = new \Stripe\StripeClient($secret_key);
+
+    // Retrieve the Checkout Session ID from the URL parameter
+    $session_id = \Drupal::request()->query->get('session_id');
+    $newRole = \Drupal::request()->query->get('roleid');
+    $isAnonymous = \Drupal::currentUser()->isAnonymous();
+
+    $session = $stripe->checkout->sessions->retrieve($session_id);
+    $email = $session->customer_details->email;
+
+    if ($session->payment_status === 'paid') {
+      // The payment is successful. You can handle any additional actions here.
+
+      // save data subscription
+      // validate purchase register in ppss_sales
+      $query = \Drupal::database()->select('ppss_sales', 's');
+      $query->condition('id_subscription', $session->subscription);
+      $num_rows = $query->countQuery()->execute()->fetchField();
+      if ($num_rows == 0) {
+        if ($isAnonymous) {
+          // It's an anonymous user. First will search about if returned email by
+          // PayPal exist, if not, trying to create an account with data returned.
+          
+          $ids = \Drupal::entityQuery('user')
+            ->condition('mail', $email)
+            ->execute();
+  
+          // Find if email exist.
+          if (!empty($ids)) {
+            // This mail already exists. Only will assign the role of the subscription
+            // plan purchased.
+            $uid = intval(current($ids));
+            try {
+              $user = \Drupal\user\Entity\User::load($uid);
+              $user->addRole($newRole);
+              $user->save();
+            } catch (\Exception $e) {
+              $errorInfo = t('Charge was made correctly but something was wrong when trying
+                to assign the new subscription plan to your account. Please contact
+                with the site administrator and explain this situation.');
+              // Show error message to the user
+              \Drupal::messenger()->addError($errorInfo);
+              \Drupal::logger('Sales')->error($errorInfo);
+              \Drupal::logger('PPSS')->error($e->getMessage());
+            }
+            $form['description'] = [
+              '#markup' => $this->t("Please login with your user account linked to this email: @email for begin use our services.", ['@email' => $email]),
+            ];
+          } else {
+            // Creates a new user with the PayPal email.
+            try {
+              // Get te user name to register from the email
+              $temp = explode("@", $email);
+              $userName = $temp[0];
+              $user = User::create();
+              $user->set('status', 1);
+              $user->setEmail($email);
+              $user->setUsername($userName);
+              $user->addRole($newRole);
+              $user->enforceIsNew();
+              $user->log;
+              $user->save();
+  
+            } catch (\Exception $e) {
+              $errorInfo = t('Charge was made correctly but something was wrong when trying
+                to create your account. Please contact with the site administrator
+                and explain this situation.');
+                // Show error message to the user
+              \Drupal::messenger()->addError($errorInfo);
+              \Drupal::logger('Sales')->error($errorInfo);
+              \Drupal::logger('PPSS')->error($e->getMessage());
+            }
+  
+            // Send confirmation email.
+            $result = array();
+            $result = _user_mail_notify('register_no_approval_required', $user);
+  
+            if ((is_null($result)) or $result == false) {
+              $message = t('There was a problem sending your email notification to @email.',
+                array('@email' => $email));
+              \Drupal::messenger()->addError($message);
+              \Drupal::logger('PPSS')->error($message);
+              $form['description'] = ['#markup' => $message,];
+  
+            } else {
+              $description = $this->t("Please review your email: @email to login details and begin use our services.", ['@email' => $email]);
+            }
+          // Get the uid of the new user.
+          $ids = \Drupal::entityQuery('user')
+            ->condition('mail', $email)
+            ->execute();
+          }
+  
+          $uid = intval(current($ids));
+  
+        } else {
+          // Only will assign the role of the subscription
+          // plan purchased to the current user
+          $uid = \Drupal::currentUser()->id();
+          try {
+            $user = \Drupal\user\Entity\User::load(\Drupal::currentUser()->id($uid));
+            $user->addRole($newRole);
+            $user->save();
+            $description = $this->t("Your user account linked to this email: @email was successfully upgraded, please enjoy.", ['@email' => $email]);
+          } catch (\Exception $e) {
+            // Show error message to the user
+            $errorInfo = t('Charge was made correctly but something was wrong when trying
+              to assign the new subscription plan to your account. Please contact
+              with the site administrator and explain this situation.');
+            \Drupal::messenger()->addError($errorInfo);
+            \Drupal::logger('stripe_payment')->error($errorInfo);
+            \Drupal::logger('stripe_payment')->error($e->getMessage());
+          }
+        }
+        
+        // Save all transaction data in DB for future reference.
+        try {
+          // Initiate missing variables to save.
+          $currentTime = \Drupal::time()->getRequestTime();
+
+          // Save the values to the database
+          // Start to build a query builder object $query.
+          // Ref.: https://www.drupal.org/docs/drupal-apis/database-api/insert-queries
+          $newSale = \Drupal::database()->insert('ppss_sales');
+
+          // Specify the fields taht the query will insert to.
+          $newSale->fields([
+            'uid',
+            'status',
+            'mail',
+            'platform',
+            'details',
+            'created',
+            'id_subscription',
+            'id_role'
+          ]);
+
+          // Set the values of the fields we selected.
+          // Note that then must be in the same order as we defined them
+          // in the $query->fields([...]) above.
+          $newSale->values([
+            $uid,
+            1,
+            $session->customer_details->email,
+            'stripe',
+            $session,
+            $currentTime,
+            $session->subscription,
+            $newRole
+          ]);
+          // Execute the query!
+          $newSale->execute();
+
+          //get data subscription
+          $getSale = \Drupal::database()->select('ppss_sales', 's');
+          $getSale->condition('id_subscription', $session->subscription);
+          $getSale->fields('s', ['id']);
+          $subscription = $getSale->execute()->fetchAll();
+
+          // insert sales details
+          $newSaleDetail = \Drupal::database()->insert('ppss_sales_details');
+          $newSaleDetail->fields(['sid', 'tax', 'price', 'total', 'created', 'event_id']);
+          $newSaleDetail->values([
+            $subscription[0]->id,
+            number_format($session->total_details->amount_tax / 100, 2),
+            number_format(($session->amount_total - $session->total_details->amount_tax) / 100, 2),
+            number_format($session->amount_total / 100, 2),
+            $currentTime,
+            0
+          ]);
+          $newSaleDetail->execute();
+          
+          \Drupal::messenger()->addMessage(t('Successful subscription purchase.'));
+        } catch (\Exception $e) {
+          // Show error message to the user
+          $errorInfo = t('Unable to save payment to DB at this time due to database error.
+            Please contact with the site administrator and explain this situation.');
+          \Drupal::messenger()->addError($errorInfo);
+          \Drupal::logger('Sales')->error($errorInfo);
+          \Drupal::logger('stripe_payment')->error($e->getMessage());
+        }
+      }
+      $description = "Thank you for your purchase!";
+    } else {
+      // The payment is not yet completed or failed. Handle accordingly.
+      $description = "Payment not completed. Please contact support.";
+    }
+    
+    return [
+      '#markup' => $description,
+    ];
+  }
+
+}
