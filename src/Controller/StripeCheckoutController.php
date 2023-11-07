a24fda3acbd2d00ce5722be58c5cc1ceddaf07db
lauraRa <ramoslaura418@gmail.com>
2023-11-07 14:29:22 -0600

    update StripeCheckoutController

--- src/Controller/StripeCheckoutController.php
+++ src/Controller/StripeCheckoutController.php
@@ -9,44 +9,51 @@ namespace Drupal\stripe_payment\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Database\Database;
+use Drupal\user\Entity\User;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class StripeCheckoutController extends ControllerBase
 {
-  public function success(){
+
+  /**
+   * Function to process payment.
+   *
+  */
+  public function successCheckout() {
     // Set your Stripe API secret key
     $config = $this->config('stripe_payment.settings');
-    $secret_key = $config->get('secret_key');
-    $stripe = new \Stripe\StripeClient($secret_key);
+    // Get stripe secret.
+    $secretKey = $config->get('secret_key');
+    // Create stripe client.
+    $stripe = new \Stripe\StripeClient($secretKey);
 
     // Retrieve the Checkout Session ID from the URL parameter
-    $session_id = \Drupal::request()->query->get('session_id');
+    $sessionId = \Drupal::request()->query->get('session_id');
     $newRole = \Drupal::request()->query->get('roleid');
     $isAnonymous = \Drupal::currentUser()->isAnonymous();
 
-    $session = $stripe->checkout->sessions->retrieve($session_id);
+    $session = $stripe->checkout->sessions->retrieve($sessionId);
     $email = $session->customer_details->email;
 
     if ($session->payment_status === 'paid') {
       // The payment is successful. You can handle any additional actions here.
-
-      // save data subscription
       // validate purchase register in ppss_sales
-      $query = \Drupal::database()->select('ppss_sales', 's');
-      $query->condition('id_subscription', $session->subscription);
-      $num_rows = $query->countQuery()->execute()->fetchField();
-      if ($num_rows == 0) {
+      $query = \Drupal::database()->select('ppss_sales', 's')->condition('id_subscription', $session->subscription);
+      $numRows = $query->countQuery()->execute()->fetchField();
+      if ($numRows == 0) {
+        // create or update user
         if ($isAnonymous) {
           // It's an anonymous user. First will search about if returned email by
-          // PayPal exist, if not, trying to create an account with data returned.
+          // Stripe exist, if not, trying to create an account with data returned.
           
           $ids = \Drupal::entityQuery('user')
+            ->accessCheck(TRUE)
             ->condition('mail', $email)
             ->execute();
   
           // Find if email exist.
           if (!empty($ids)) {
             // This mail already exists. Only will assign the role of the subscription
-            // plan purchased.
             $uid = intval(current($ids));
             try {
               $user = \Drupal\user\Entity\User::load($uid);
@@ -58,14 +65,12 @@ class StripeCheckoutController extends ControllerBase
                 with the site administrator and explain this situation.');
               // Show error message to the user
               \Drupal::messenger()->addError($errorInfo);
-              \Drupal::logger('Sales')->error($errorInfo);
-              \Drupal::logger('PPSS')->error($e->getMessage());
+              \Drupal::logger('stripe payment')->error($errorInfo);
+              \Drupal::logger('stripe payment')->error($e->getMessage());
             }
-            $form['description'] = [
-              '#markup' => $this->t("Please login with your user account linked to this email: @email for begin use our services.", ['@email' => $email]),
-            ];
+            $fdescription = $this->t("Please login with your user account linked to this email: @email for begin use our services.", ['@email' => $email]);
           } else {
-            // Creates a new user with the PayPal email.
+            // Creates a new user with the Stripe email.
             try {
               // Get te user name to register from the email
               $temp = explode("@", $email);
@@ -85,28 +90,27 @@ class StripeCheckoutController extends ControllerBase
                 and explain this situation.');
                 // Show error message to the user
               \Drupal::messenger()->addError($errorInfo);
-              \Drupal::logger('Sales')->error($errorInfo);
-              \Drupal::logger('PPSS')->error($e->getMessage());
+              \Drupal::logger('stripe payment')->error($errorInfo);
+              \Drupal::logger('stripe payment')->error($e->getMessage());
             }
   
             // Send confirmation email.
             $result = array();
             $result = _user_mail_notify('register_no_approval_required', $user);
   
-            if ((is_null($result)) or $result == false) {
+            if ((is_null($result)) || $result == false) {
               $message = t('There was a problem sending your email notification to @email.',
                 array('@email' => $email));
               \Drupal::messenger()->addError($message);
-              \Drupal::logger('PPSS')->error($message);
-              $form['description'] = ['#markup' => $message,];
-  
+              \Drupal::logger('stripe payment')->error($message);
             } else {
               $description = $this->t("Please review your email: @email to login details and begin use our services.", ['@email' => $email]);
             }
-          // Get the uid of the new user.
-          $ids = \Drupal::entityQuery('user')
-            ->condition('mail', $email)
-            ->execute();
+            // Get the uid of the new user.
+            $ids = \Drupal::entityQuery('user')
+              ->accessCheck(TRUE)
+              ->condition('mail', $email)
+              ->execute();
           }
   
           $uid = intval(current($ids));
@@ -140,7 +144,6 @@ class StripeCheckoutController extends ControllerBase
           // Start to build a query builder object $query.
           // Ref.: https://www.drupal.org/docs/drupal-apis/database-api/insert-queries
           $newSale = \Drupal::database()->insert('ppss_sales');
-
           // Specify the fields taht the query will insert to.
           $newSale->fields([
             'uid',
@@ -154,8 +157,7 @@ class StripeCheckoutController extends ControllerBase
           ]);
 
           // Set the values of the fields we selected.
-          // Note that then must be in the same order as we defined them
-          // in the $query->fields([...]) above.
+          // Note that then must be in the same order as we defined them in the $query->fields([...]) above.
           $newSale->values([
             $uid,
             1,
@@ -169,17 +171,17 @@ class StripeCheckoutController extends ControllerBase
           // Execute the query!
           $newSale->execute();
 
-          //get data subscription
+          //get the ppss_sales data inserted above
           $getSale = \Drupal::database()->select('ppss_sales', 's');
           $getSale->condition('id_subscription', $session->subscription);
           $getSale->fields('s', ['id']);
-          $subscription = $getSale->execute()->fetchAll();
+          $subscription = $getSale->execute()->fetchAssoc();
 
           // insert sales details
           $newSaleDetail = \Drupal::database()->insert('ppss_sales_details');
           $newSaleDetail->fields(['sid', 'tax', 'price', 'total', 'created', 'event_id']);
           $newSaleDetail->values([
-            $subscription[0]->id,
+            $subscription['id'],
             number_format($session->total_details->amount_tax / 100, 2),
             number_format(($session->amount_total - $session->total_details->amount_tax) / 100, 2),
             number_format($session->amount_total / 100, 2),
@@ -198,15 +200,20 @@ class StripeCheckoutController extends ControllerBase
           \Drupal::logger('stripe_payment')->error($e->getMessage());
         }
       }
+
       $description = "Thank you for your purchase!";
+      \Drupal::messenger()->addMessage($description);
+      return new RedirectResponse($config->get('success_url'));
+
     } else {
       // The payment is not yet completed or failed. Handle accordingly.
       $description = "Payment not completed. Please contact support.";
+      \Drupal::messenger()->addMessage($description);
+      return new RedirectResponse($config->get('cancel_url'));
     }
-    
-    return [
+    /*return [
       '#markup' => $description,
-    ];
+    ];*/
   }
 
 }
